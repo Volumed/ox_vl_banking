3d32714804f20e200ce4ed827d08b61955858729
arl0fonseca <thedondada7801@gmail.com>
2022-08-15 22:16:43 -0400

    refactor(*): move props

--- client/main.lua
+++ client/main.lua
@@ -1,17 +1,8 @@
-local ATMProps = {
-	`prop_atm_01`,
-	`prop_atm_02`,
-	`prop_atm_03`,
-	`prop_fleeca_atm`,
-	`v_5_b_atm1`,
-	`v_5_b_atm2`
-}
-
 RegisterNetEvent("openBank", function()
-	lib.callback('vl_banking:getData', 1000, function(data)
+	lib.callback("vl_banking:getData", 1000, function(data)
 		SetNuiFocus(true, true)
 		SendNUIMessage({
-			type = 'open',
+			type = "open",
 			money = data.money,
 			balance = data.balance,
 			players = json.encode(data.players),
@@ -22,21 +13,21 @@ end)
 if config.target.ox_target then
 	exports.ox_target:addGlobalObject({
 		{
-			icon = 'fas fa-credit-card',
-			label = 'Open ATM',
-			event = 'openBank',
+			icon = "fas fa-credit-card",
+			label = "Open ATM",
+			event = "openBank",
 			canInteract = function(entity, coords, distance)
 				return true
-			end
-		}
+			end,
+		},
 	})
 elseif config.target.qtarget then
-	exports.qtarget:AddTargetModel(ATMProps, {
-		options = {{
-			icon = 'fas fa-credit-card',
-			label = 'Use ATM',
-			event = 'openBank'
-		}},
-		distance = 1.5
+	exports.qtarget:AddTargetModel(config.atmprops.props, {
+		options = { {
+			icon = "fas fa-credit-card",
+			label = "Use ATM",
+			event = "openBank",
+		} },
+		distance = 1.5,
 	})
 end
